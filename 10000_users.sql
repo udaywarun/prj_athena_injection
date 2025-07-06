13e6207706d385c1e1ff733e9af9dc4e29877c90
udaywarun <57788276+udaywarun@users.noreply.github.com>
2025-07-06 14:23:22 +0530

    Update 10000_users.sql

--- 10000_users.sql
+++ 10000_users.sql
@@ -1,10 +1,5 @@
 
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
